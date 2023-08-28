5bde1ce214383a84aae91f24204d86a529df9b53
Dhruv Bhatia <dbhatia03@gmail.com>
2023-08-28 22:05:49 +0530

    button refactor

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,15 @@ const Button = (props) => {
 	const { name } = props;
 	return (
 		<div>
-			<button className="px-5 py-2 m-2 bg-gray-200 rounded-lg">{name}</button>
+			<button
+				className={
+					name === "All"
+						? "bg-gray-300 px-5 py-2 m-2  rounded-lg"
+						: "bg-gray-200 px-5 py-2 m-2  rounded-lg hover:bg-gray-300"
+				}
+			>
+				{name}
+			</button>
 		</div>
 	);
 };
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -4,7 +4,7 @@ import VideoContainer from "./VideoContainer";
 
 const MainContainer = () => {
 	return (
-		<div className="w-11/12">
+		<div className="">
 			<ButtonList />
             <VideoContainer/>
 		</div>
